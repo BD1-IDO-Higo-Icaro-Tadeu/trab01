3bc5942fd0c4bd4359473d010c673996ad7ab60c
Tadeujr <tadeu_junior@outlook.com>
2017-11-01 00:06:30 -0200

    correçaõ

--- arquivos/criacao de tabela ido.sql
+++ arquivos/criacao de tabela ido.sql
@@ -232,6 +232,20 @@ insert into cartao (nome_cartao,numero_cartao,cvv,validade,cliente_fid) values
 ('Elimar Lolzin',	'23451234567',	765,	'12-05-23',	999999),
 ('Julio Faker',	'23451789654',	567,	'11-09-21',	123456);
 
+/*AVALIACAO*/
+insert into avalicao (cliente_fid,comentario,servico_fid,nota,prestador_fid) values
+(111111,	'blabla',	10,3.5, 999999),
+(222222,	'blabla',	11,4.9,	123456),
+(333333,	'blabla',	7,3.1,	666666),
+(444444,	'blabla',	7,2.3,	666666),
+(555555,	'blabla',	11,3.5,	123456),
+(666666,	'blabla',	9,2.0,	888888),
+(777777,	'blabla',	11,1.0,	123456),
+(888888,	'blabla',	10,3.6,	999999),
+(999999,	'blabla',	7,1.1,	666666),
+(123456,	'blabla',	3,2.6,	333333);
+
+
 /*SERVICO CONTRATADO*/
 insert into servico_contratado (usuario_fid,servico_fid,prestador_fid,servico_descricao_fid) values
 
