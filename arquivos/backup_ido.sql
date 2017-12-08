68ef9275b662de9cc39b114d98fd9f56f2b3e7e2
Higor Rebello <31069307+higorrebello@users.noreply.github.com>
2017-12-08 16:29:04 -0200

    Correções Script
    Problema no INSERT INTO pessoa resolvido.

--- arquivos/backup_ido.sql
+++ arquivos/backup_ido.sql
@@ -190,17 +190,17 @@ insert into endereco (cep_fid,usuario_fid) values
 (89641321,999999),
 (12365589,123456);
 
-insert into pessoa (nome,email, rg, senha, num_casa,cpf_id,endereco_fid, usuario,contato_fid,servico_fid,descricao_fid) values 
-('Tadeu Junior',	'tadeu@gmail.com',	223344,	'11111',	55,111111,1,'Tadeu',2,1),
-('Yan de Paula','yan@gmail.com',887766,	'22222',	456,222222,4,'Yan',1,2),
-('Ewerson Vieira',	'ewerson@gmail.com',	323456,	'33333',	89,333333,5,'Ewerson',3,3),
-('Lucas Gomes Irinel',	'luca-irinel@gmail.com',	265260,	'44444',	86,444444,6,'Lucas',4,4),
-('Icaro Duarte'	,'icaro@gmail.com',	223344,	'55555',	47,555555,7,'Icaro',1,5),
-('David Vilaca', 'david@gmail.com',	445566,	'66666',	22,666666,8,'David',7,6),
-('Leandro Goias', 'leandro@gmail.com',	778899,	'77777',	36,777777,9,'Leandro',9,7),
-('Luiz Melodia','luiz@gmail.com',	345234,	'88888',	35,888888,3,'Luiz',9,8),
-('Elimar Lolzin',	'elimar@gmail.com',	567533, '99999',	327,999999,1,'Elimar',10,9),
-('Julio Faker',	'julio@gmail.com',	456789,	'12345',	641,123456,2,'Julio',11,10);
+insert into pessoa (nome, email, rg, senha, num_casa,cpf_id,endereco_fid, usuario,contato_fid,servico_fid,descricao_fid) values 
+('Tadeu Junior',	'tadeu@gmail.com',	223344,	'11111',	55,111111,1,'Tadeu',2,1,1),
+('Yan de Paula','yan@gmail.com',887766,	'22222',	456,222222,4,'Yan',1,2,2),
+('Ewerson Vieira',	'ewerson@gmail.com',	323456,	'33333',	89,333333,5,'Ewerson',3,3,3),
+('Lucas Gomes Irinel',	'luca-irinel@gmail.com',	265260,	'44444',	86,444444,6,'Lucas',4,4,4),
+('Icaro Duarte'	,'icaro@gmail.com',	223344,	'55555',	47,555555,7,'Icaro',1,5,5),
+('David Vilaca', 'david@gmail.com',	445566,	'66666',	22,666666,8,'David',7,6,6),
+('Leandro Goias', 'leandro@gmail.com',	778899,	'77777',	36,777777,9,'Leandro',9,7,7),
+('Luiz Melodia','luiz@gmail.com',	345234,	'88888',	35,888888,3,'Luiz',9,8,8),
+('Elimar Lolzin',	'elimar@gmail.com',	567533, '99999',	327,999999,1,'Elimar',10,9,9),
+('Julio Faker',	'julio@gmail.com',	456789,	'12345',	641,123456,2,'Julio',11,10,10);
 
 
 /*CONTATO*/
@@ -269,7 +269,7 @@ insert into  servico_descricao (usuario_fid,servico_opcao,unidade,valor,servico_
 (777777,	'evento',	'a combinar',	NULL,	8,	'foto garçon','Sou amigo de todos'),
 (888888,	NULL,	NULL,	NULL,	1,	NULL,NULL),
 (999999,	'dia',	'valor fixo',	'300,00',	3,	'foto arroz','Tempero de Mae'),
-(123456,	'hora',	'a combinar',	NULL,	11,	'foto banheiro','Me contratae e nao se arrependa');
+(123456,	'hora',	'a combinar',	NULL,	11,	'foto banheiro','Me contrate e nao se arrependa');
 
 /*AVALIACAO*/
 insert into avaliacao (cliente_fid,comentario,servico_fid,nota,prestador_fid) values
